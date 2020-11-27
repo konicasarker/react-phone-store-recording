0df63e98ac6c6bfa38f39b639f6cfe9856d5e331
konica.sarker <konica.sarker@bonial.com>
2020-11-27 01:13:18 +0100

    warning removed

--- src/components/Details.js
+++ src/components/Details.js
@@ -8,7 +8,7 @@ export default class Details extends Component {
         return (
             <ProductConsumer>
                 {(value)=>{
-                    const {id, company,img,info,price,title,inCart} = 
+                    const {id,img,info,price,title,inCart} = 
                     value.detailProduct;
                     return (
                         <div className="container py-5">
